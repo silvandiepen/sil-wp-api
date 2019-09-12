f63c567550723ca38e3b19807c76b9f8c20c6e22
Sil van Diepen <me@silvandiepen.com>
2019-09-12 15:18:17 +0200

    update menu and get children

--- functions/api/navigation.php
+++ functions/api/navigation.php
@@ -1,60 +1,86 @@
 <?php
 
-function get_navbar_items($items, $parent = 0) {
-	
-	// Define a menu group
-	$menu = [];
+function get_menu_item_children($items, $parent = 0){
+	$newitems = [];
+	foreach ($items as $key => $value){
 
-	// Get all items for this menu
-	$i = 0;
-	
-	// Go through the items and add them, start with 0 and go through all children.
-	foreach($items as $item) {
-		if($item->menu_item_parent == $parent){
-			$a = new stdClass();
-			$a->title = $item->title;
-			$a->url = $item->url;
-			$a->name = $item->post_name;
-			$a->parent = (int)$item->menu_item_parent;
-			$a->order = (int)$item->menu_order;
-			if($item->classes[0] !== ""){
-				$a->classes = $item->classes;
-			}
-// 			$a->all = $item;
-			
-			// Get the children by calling this same function. 
-// 			$a->children = get_navbar_items($items, $item->menu_item_parent, true);
-			$menu[] = $a;
+		if($value->parent == $parent){
+			$value->children = get_menu_item_children($items, $value->ID);	
+
+			$newitems[] = $value;	
 		}
+
 	}
-	
-	// Return the menu
-	return $menu;
+	if(count($newitems) < 1){
+		return false;	
+	} else {
+		return $newitems; 
+	}
 }
 
-function get_all_nav() {   
-	$menus = get_terms( 'nav_menu', array( 'hide_empty' => true ) ); 
-	
-	$all_menus = [];
-	
-	
-	foreach ($menus as $item) {
-		
-		$menu = new stdClass();
-		$menu->name = $item->slug;
-		$menu->menu = get_navbar_items(wp_get_nav_menu_items($item->slug));
-		$all_menus[] = $menu;
-	}
-    
-    return $all_menus;
 
+function get_nav_menu_items_by_location( $location, $args = [] ) {
+ 
+    // Get all locations
+    $locations = get_nav_menu_locations();
+
+
+    // Get object id by location
+    $object = wp_get_nav_menu_object( $locations[$location] );
+ 
+    // Get menu items by menu name
+    $menu_items = wp_get_nav_menu_items( $object->name, $args );
+    
+    $items = [];
+    
+    foreach ($menu_items as $key => $value){
+	    $item = new stdClass();
+	    $item->ID = (int)$value->ID;
+	    $item->title = $value->post_title;
+	    $item->name = $value->post_name;
+		$item->link = $value->url;
+		$item->classes = count($value->classes)>1 ? $value->classes : false;
+		$item->parent = (int)$value->menu_item_parent;
+		$item->order = $value->menu_order;
+		$item->fields = get_fields($value->ID);
+	    $items[] = $item;
+    }
+ 
+    // Return menu post objects
+    return get_menu_item_children($items);
+}
+
+
+function get_menus(){
+	$registered_menus = get_registered_nav_menus();
+
+	$menus = [];	
+
+	foreach($registered_menus as $key => $value) {
+		$menu = new stdClass();
+		$menu->name = $key;
+		$menu->title = $value;
+		$menu->items = get_nav_menu_items_by_location($key);
+
+	    $menus[] = $menu; 
+	}
+	
+	return $menus;
+}
+
+function get_single_menu(){
+	return get_nav_menu_items_by_location($_GET['menu']);
 }
 
 add_action( 'rest_api_init', function () {
 
 	register_rest_route( 'navigation', '/all', array(
         'methods' => 'GET',
-        'callback' => 'get_all_nav',
+        'callback' => 'get_menus',
+    ) );	
+    register_rest_route( 'navigation', '/menu', array(
+        'methods' => 'GET',
+        'callback' => 'get_single_menu',
     ) );
               
 });
